416f7baf285ebff2b771ff4fa5b9e306aed47f82
JpatrickR <patrickramirez886@gmail.com>
2021-03-08 15:03:01 +0800

    string statement

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ name_input = input("Enter name: ")
 age_input = int(input("Enter age: "))
 
 print(f"Hi my name is {name_input}. I am {age_input} years old.")
-
+# if else statement
 if age_input < 19:
     print("You're a Teenager")
 else:
     print("You're an Adult")
+
+# string
+str1 = "Hello"
+str2 = "Word"
+
+print(str1 + str2)
